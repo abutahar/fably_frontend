a264b501f2ae7f37fe72b415696bb851c3de1b65
abutahar <mdabutaharmondol@gmail.com>
2023-10-18 19:00:35 +0600

    mobile header redesign

--- src/Components/Pages/Common/Header/Navbar.jsx
+++ src/Components/Pages/Common/Header/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
           {/* logo */}
           <div className="mr-auto text-2xl font-bold font-mono">Fably</div>
 
-          {/* search icon */}
+          {/* home icon */}
           <div className="rounded-full p-1 hover:shadow-md">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -66,11 +66,10 @@ const Navbar = () => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
               />
             </svg>
           </div>
-
           {/* cart icon */}
           <div className="rounded-full p-1 hover:shadow-md">
             <svg
@@ -89,8 +88,8 @@ const Navbar = () => {
             </svg>
           </div>
 
-          {/* profile icon */}
-          <div className="rounded-full p-1  hover:shadow-md">
+          {/* store icon */}
+          <div className="rounded-full p-1 hover:shadow-md">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -102,7 +101,25 @@ const Navbar = () => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+          </div>
+
+          {/* search icon */}
+          <div className="rounded-full p-1 hover:shadow-md">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
               />
             </svg>
           </div>
